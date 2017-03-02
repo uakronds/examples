071af98326d709643ef59f989986d7ec3ee022ae
Anthony Deeter <aed27@zips.uakron.edu>
2017-03-02 18:22:53 -0500

    avl tree with insert complete

--- AVL/avl.cpp
+++ AVL/avl.cpp
@@ -1,14 +1,22 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 #include "avl_tree.hpp"
 
 int main() {
-  AVLTree<int>* avl = new AVLTree<int>;
 
-  avl->insert(10);
-  avl->insert(5);
-  avl->insert(20);
-  avl->insert(50);
+  srand(time(0));
+
+  AVLTree<int>* avl = new AVLTree<int>;
+  /*
+  for (int i = 0; i < 16; ++i)
+    avl->insert(rand() % 100);
+  */
+
+  for (auto x : { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }) 
+    avl->insert(x);
 
   std::cout << avl->serialize() << std::endl;
+
 }
--- AVL/avl_tree.hpp
+++ AVL/avl_tree.hpp
@@ -1,6 +1,7 @@
 #ifndef AVL_TREE_HPP
 #define AVL_TREE_HPP
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -17,11 +18,16 @@ struct AVLNode {
 
 template <class T>
 class AVLTree {
-private:
+public:
   AVLNode<T>* root = nullptr;
 
   void insert(AVLNode<T>*, T);
+  void update_height(AVLNode<T>*);
+  int  check_balance(AVLNode<T>*);
   void left_rotate(AVLNode<T>*);
+  void right_rotate(AVLNode<T>*);
+  void double_left_rotate(AVLNode<T>*);
+  void double_right_rotate(AVLNode<T>*);
   void serialize(AVLNode<T>*, std::stringstream&);
 public:
   AVLTree() {}
@@ -33,11 +39,26 @@ public:
 };
 
 template <class T>
+void AVLTree<T>::update_height(AVLNode<T>* n) {
+  if (!n)
+    return;
+
+  n->height = 1 + std::max(n->left ? n->left->height : -1, n->right ? n->right->height : -1);
+}
+
+template <class T>
+int AVLTree<T>::check_balance(AVLNode<T>* n) {
+  return (n->left ? n->left->height : -1) - (n->right ? n->right->height : -1);
+}
+
+template <class T>
 void AVLTree<T>::left_rotate(AVLNode<T>* n) {
   AVLNode<T>* p = n;
   AVLNode<T>* m = n->right;
   p->right = m->left;
-  m->left->parent = p;
+
+  if (m->left)
+    m->left->parent = p;
 
   m->parent = n->parent;
 
@@ -48,6 +69,43 @@ void AVLTree<T>::left_rotate(AVLNode<T>* n) {
     root = m;
   else
     m->data < m->parent->data ? m->parent->left = m : m->parent->right = m;
+
+  update_height(p);
+  update_height(m);
+}
+
+template <class T>
+void AVLTree<T>::right_rotate(AVLNode<T>* n) {
+  AVLNode<T>* p = n;
+  AVLNode<T>* m = n->left;
+  p->left = m->right;
+  if (m->right)
+    m->right->parent = p;
+
+  m->parent = n->parent;
+
+  m->right = p;
+  p->parent = m;
+
+  if (!m->parent)
+    root = m;
+  else
+    m->data < m->parent->data ? m->parent->left = m : m->parent->right = m;
+
+  update_height(p);
+  update_height(m);
+}
+
+template <class T>
+void AVLTree<T>::double_left_rotate(AVLNode<T>* n) {
+  right_rotate(n->right);
+  left_rotate(n);
+}
+
+template <class T>
+void AVLTree<T>::double_right_rotate(AVLNode<T>* n) {
+  left_rotate(n->left);
+  right_rotate(n);
 }
 
 template <class T>
@@ -77,7 +135,21 @@ void AVLTree<T>::insert(AVLNode<T>* n, T t) {
       n->right = new AVLNode<T>(t, n);
     }
   }
-
+  update_height(n);
+  int bf = check_balance(n);
+  if (bf < -1) {
+   if (check_balance(n->right) > 0) {
+      double_left_rotate(n);
+   } else {
+     left_rotate(n);
+   }
+  } else if (bf > 1) {
+    if (check_balance(n->left) < 0) {
+      double_right_rotate(n);
+    } else {
+      right_rotate(n);
+    }
+  } 
 } 
  
 
@@ -95,7 +167,7 @@ void AVLTree<T>::serialize(AVLNode<T>* n, std::stringstream& s) {
     return;
   }
 
-  s << n->data << " ";
+  s << "[" << n->data << "|" << n->height << "] ";
   serialize(n->left, s);
   serialize(n->right, s);
 }
